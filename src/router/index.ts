79527d186d2a2b2f89a932c816e6f28dfae11677
Revati Gawas <gawas.revati@gmail.com>
2022-05-30 19:16:07 +0200

    Bugfix: Edit profile page not working.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ProfileView from '../views/ProfileView.vue'
-import EditProfileView from '../views/EditProfileView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EditProfileViewVue from "../views/EditProfileView.vue";
+import ProfileViewVue from "../views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'profile',
-      component: ProfileView
+      path: "/",
+      name: "profile",
+      component: ProfileViewVue,
     },
     {
-      path: '/edit-profile',
-      name: 'edit-profile',
-      component: () => EditProfileView
-    }
-  ]
-})
+      path: "/edit-profile",
+      name: "edit-profile",
+      component: EditProfileViewVue,
+    },
+  ],
+});
 
-export default router
+export default router;
